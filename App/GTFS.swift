ecd65a02a85aed5484350e0d39fa5aad558e5127
Patrick Sheehan <patrick@cerebusenergy.com>
2022-12-16 18:22:13 -0800

    rip sqlite version

--- App/GTFS.swift
+++ App/GTFS.swift
@@ -1,12 +1,6 @@
 // Written in collaboration with chatGPT! But sort of barely...
 
 import Foundation
-import SwiftCSV
-
-protocol GTFSObject {
-    static var fileName: String { get }
-    init(csvRow: [String: String])
-}
 
 struct Stop {
     let stopID: String
@@ -16,62 +10,13 @@ struct Stop {
     var distanceMiles: Double?
 }
 
-extension Stop: GTFSObject {
-    static let fileName = "stops.txt"
-    
-    init(csvRow: [String: String]) {
-        self.stopID = csvRow["stop_id"]!
-        self.stopName = csvRow["stop_name"]!
-        self.stopLat = Double(csvRow["stop_lat"]!)!
-        self.stopLon = Double(csvRow["stop_lon"]!)!
-        self.parentStation = csvRow["parent_station"]!
-        self.distanceMiles = nil
-    }
-}
-
-struct Trip: GTFSObject {
-    static let fileName = "trips.txt"
-    
+struct Trip {
     let routeID: String
     let serviceID: String?
     let tripID: String
-    
-    init(csvRow: [String: String]) {
-        tripID = csvRow["trip_id"]!
-        routeID = csvRow["route_id"]!
-        serviceID = csvRow["service_id"]
-    }
 }
 
-struct Route: GTFSObject {
-    static let fileName = "routes.txt"
-    
+struct Route {
     let routeID: String
     let routeLongName: String
-        
-    init(csvRow: [String: String]) {
-      routeID = csvRow["route_id"]!
-      routeLongName = csvRow["route_long_name"]!
-    }
-}
-
-
-class GTFS {
-    let routes: [Route]
-    let stops: [Stop]
-    let trips: [Trip]
-    
-    init(gtfsFolderUrl: URL) {
-        func structFromUrl<GTFSType>(gtfsFolderUrl: URL, type: GTFSType.Type) -> [GTFSType] where GTFSType : GTFSObject{
-            var returnList: [GTFSType] = []
-            let rows = try! NamedCSV(url: gtfsFolderUrl.appendingPathComponent(type.fileName), delimiter: CSVDelimiter.comma, loadColumns: false).rows
-            for row in rows {
-                returnList.append(type.init(csvRow: row))
-            }
-            return returnList
-        }
-        self.stops = structFromUrl(gtfsFolderUrl: gtfsFolderUrl, type: Stop.self)
-        self.trips = structFromUrl(gtfsFolderUrl: gtfsFolderUrl, type: Trip.self)
-        self.routes = structFromUrl(gtfsFolderUrl: gtfsFolderUrl, type: Route.self)
-    }
 }
